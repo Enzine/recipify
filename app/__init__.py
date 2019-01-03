8e76205ede33efa0c596522a69b6fe7ea1ced116
Sonja Mäkipaja <sonjamak@cs.helsinki.fi>
2019-01-03 14:27:53 +0200

    add login/logout functionalities

--- app/__init__.py
+++ app/__init__.py
@@ -3,18 +3,35 @@ from flask import Flask
 app = Flask(__name__)
 
 from flask_sqlalchemy import SQLAlchemy
-# Kolme vinoviivaa kertoo, tiedosto sijaitsee tämän sovelluksen 
-# tiedostojen kanssa samassa paikassa
+
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///recipes.db"
 
-# Pyydetään SQLAlchemyä tulostamaan kaikki SQL-kyselyt
+# Print sql queries
 app.config["SQLALCHEMY_ECHO"] = True
 
-# Luodaan db-olio, jota käytetään tietokannan käsittelyyn
+# Create db object
 db = SQLAlchemy(app)
 
 from app import views
 
 from app.recipes import models, views
-# Luodaan lopulta tarvittavat tietokantataulut
+from app.auth import models, views
+
+# Login
+from app.auth.models import User
+from os import urandom
+app.config["SECRET_KEY"] = urandom(32)
+
+from flask_login import LoginManager
+login_manager = LoginManager()
+login_manager.init_app(app)
+
+login_manager.login_view = "auth_login"
+login_manager.login_message = "Please login to use this functionality."
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(user_id)
+
+# Create db tables
 db.create_all()
---
+++ app/auth/views.py
@@ -0,0 +1,28 @@
+# coding=utf-8
+from flask import render_template, request, redirect, url_for
+from flask_login import login_user, logout_user
+
+from app import app
+from app.auth.models import User
+from app.auth.forms import LoginForm
+
+@app.route("/auth/login", methods = ["GET", "POST"])
+def auth_login():
+    if request.method == "GET":
+        return render_template("auth/login.html", form = LoginForm())
+
+    form = LoginForm(request.form)
+    # validations
+
+    user = User.query.filter_by(username=form.username.data, password=form.password.data).first()
+    if not user:
+        return render_template("auth/login.html", form = form,
+                               error = "No such username or password")
+
+    login_user(user)
+    return redirect(url_for("index"))    
+
+@app.route("/auth/logout")
+def auth_logout():
+    logout_user()
+    return redirect(url_for("index"))    
