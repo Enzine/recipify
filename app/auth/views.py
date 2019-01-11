4e7fd3901fea5d901acf461e81cb5beed414d8e1
Sonja Mäkipaja <sonjamak@cs.helsinki.fi>
2019-01-11 23:30:25 +0200

    fix bug where user can register with existing username if the password is different

--- app/auth/views.py
+++ app/auth/views.py
@@ -28,7 +28,8 @@ def auth_register():
 
     form = LoginForm(request.form)
 
-    user = User.query.filter_by(username=form.username.data, password=form.password.data).first()
+    user = User.query.filter_by(username=form.username.data).first()
+
     if not user:
         user = User(form.username.data, form.password.data)
 
