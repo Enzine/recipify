48947545a4692cd8be748b062c4cc635869b438e
Sonja Mäkipaja <sonjamak@cs.helsinki.fi>
2019-01-11 20:38:33 +0200

    replace login_required with role_required in recipes views.py

--- app/recipes/views.py
+++ app/recipes/views.py
@@ -1,6 +1,6 @@
-from app import app, db
+from app import app, db, role_required
 from flask import render_template, request, url_for, redirect
-from flask_login import login_required, current_user
+from flask_login import current_user
 from app.recipes.models import Recipe
 from app.recipes.forms import RecipeForm
 from app.comments.forms import CommentForm
@@ -23,21 +23,21 @@ def recipes_show(recipe_id):
 # GET /recipes/new
 # Shows form to create a new recipe.
 @app.route("/recipes/new/")
-@login_required
+@role_required(role="USER")
 def recipes_form():
     return render_template("recipes/new.html", form = RecipeForm())
 
 # GET /recipes/<recipe_id>/edit
 # Shows form to edit a chosen recipe.
 @app.route("/recipes/<recipe_id>/edit/")
-@login_required
+@role_required(role="USER")
 def recipes_edit_form(recipe_id):
     return render_template("recipes/edit.html", recipe = Recipe.query.get(recipe_id), form = RecipeForm(obj=Recipe.query.get(recipe_id)))
 
 # PUT /recipes/<recipe_id>/like
 # Edits the given recipy by adding a like.
 @app.route("/recipes/<recipe_id>/like/", methods=["POST"])
-@login_required
+@role_required(role="USER")
 def recipes_add_like(recipe_id):
 
     r = Recipe.query.get(recipe_id)
@@ -49,7 +49,7 @@ def recipes_add_like(recipe_id):
 # POST /recipes
 # Creates a new recipe.
 @app.route("/recipes/", methods=["POST"])
-@login_required
+@role_required(role="USER")
 def recipes_create():
     form = RecipeForm(request.form)
 
@@ -72,7 +72,7 @@ def recipes_create():
 # PUT /recipes/<recipe_id>/edit
 # Edits the given recipe.
 @app.route("/recipes/<recipe_id>/edit/", methods=["POST"])
-@login_required
+@role_required(role="USER")
 def recipes_edit(recipe_id):
     form = RecipeForm(request.form)
 
@@ -95,7 +95,7 @@ def recipes_edit(recipe_id):
 # DELETE /recipes/<recipe_id>/delete
 # Deletes a chosen recipe entity from database.
 @app.route("/recipes/<recipe_id>/delete/", methods=["POST"])
-@login_required
+@role_required(role="USER")
 def recipes_remove(recipe_id):
     r = Recipe.query.get(recipe_id)
     
